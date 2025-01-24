f8793bb028e55dfdc73720bb3f6c7e742e2e12fc
LibRusty <alibaevrustam1024@gmail.com>
2025-01-24 19:16:48 +0700

    Done

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Добавьте поддержку контроллеров и представлений
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Настройка маршрутов
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
